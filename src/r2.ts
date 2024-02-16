57af8cced84272b805708ae67a9dd718bc4673d5
Henrique Antonio <henriqueolivoantonio@outlook.com>
2024-02-16 17:00:40 -0300

    feat: env-checker, r2 upload and dumping

---
+++ src/r2.ts
@@ -0,0 +1,39 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+import { tmpFolder } from './helpers';
+
+const R2Client = new S3Client({
+  region: 'us-east-1',
+  endpoint: process.env.R2_ENDPOINT_URL,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY
+  },
+});
+
+export const uploadToR2 = async (filename: string): Promise<void> => {
+  console.time(`--> Uploading ${filename} to R2...`)
+
+  const file = Bun.file(path.resolve(tmpFolder, filename));
+
+  await R2Client.send(
+    new PutObjectCommand({
+      Bucket: process.env.R2_BUCKET_NAME,
+      Key: filename,
+      ACL: 'public-read',
+      Body: file,
+    }),
+  )
+
+  console.timeEnd(`--> Uploading ${filename} to R2...`)
+}
+
+export const deleteFile = async (filename: string): Promise<void> => {
+  console.time(`--> Deleting ${filename}...`);
+
+  await fs.unlink(path.resolve(tmpFolder, filename));
+
+  console.timeEnd(`--> Deleting ${filename}...`);
+}
